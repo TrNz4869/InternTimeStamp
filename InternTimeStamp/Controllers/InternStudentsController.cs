5baf8c63976e23278db729c0953fff1a37eaaaab
ธีรยุทธ นุชผดุง <6210110149@psu.ac.th>
2022-06-08 20:08:40 +0700

    add intern students page

--- InternTimeStamp/Controllers/InternStudentsController.cs
+++ InternTimeStamp/Controllers/InternStudentsController.cs
@@ -28,16 +28,15 @@ namespace InternTimeStamp.Controllers
 
             // query intern
             connection.Open();
-            //connection.Query<Intern>("SELECT TOP (1000) [Name] ,[University_Code] FROM [Intern] ORDER BY [LastModifyDate] ASC").ToList();
+            //connection.Query<InternStudent>("SELECT TOP (1000) [Name] ,[University_code] FROM [Intern] ORDER BY [LastModifyDate] ASC").ToList();
 
-            SqlCommand queryInternCmd = new SqlCommand("SELECT [Name] ,[University_Code] FROM [Intern] ORDER BY [LastModifyDate] ASC", connection);
+            SqlCommand queryInternCmd = new SqlCommand("SELECT [Name], [University_code] FROM [Intern] ORDER BY [LastModifyDate] ASC", connection);
             dr = queryInternCmd.ExecuteReader();
             while (dr.Read())
             {
                 viewModel.InternStudentLists.Add(new InternStudent() { Name = dr.GetString(0), University = dr.GetString(1) });
             }
             connection.Close();
-
 
             // query university
             connection.Open();
@@ -56,102 +55,128 @@ namespace InternTimeStamp.Controllers
         }
 
         // add
-        //[HttpPost]
-        //public ActionResult Index(Intern intern_list)
-        //{
-        //    var listIntern = new List<Intern>();
-        //    string connectionstring = configuration.GetConnectionString("defaultConnectionString");
-        //    SqlConnection connection = new SqlConnection(connectionstring);
+        [HttpPost]
+        public ActionResult Index(InternStudent intern_student)
+        {
+            var listInternStudent = new List<InternStudent>();
+            string connectionstring = configuration.GetConnectionString("defaultConnectionString");
+            SqlConnection connection = new SqlConnection(connectionstring);
 
-        //    connection.Open();
-        //    connection.Execute("INSERT INTO Intern(Name, University_Code, LastModifyDate) VALUES(@Name, @University_Code, getdate())",
-        //        new
-        //        {
-        //            Name = intern_list.Name,
-        //            University_Code = intern_list.University_Code,
-        //            LastModifyDate = intern_list.LastModifyDate
-        //        });
+            connection.Open();
+            connection.Execute("INSERT INTO [Intern] ([Name], [University_code], [LastModifyDate]) VALUES (@Name, @University_code, getdate())",
+                new
+                {
+                    Name = intern_student.Name,
+                    University_code = intern_student.University,
+                    LastModifyDate = intern_student.LastModifyDate
+                });
 
-        //    //SqlCommand insertCmd = new SqlCommand("INSERT INTO Intern(Name, University_Code, LastModifyDate) VALUES(@Name, @University_Code, getdate())");
-        //    //insertCmd.Connection = connection;
-        //    //insertCmd.Parameters.AddWithValue("@Name", intern_list.Name);
-        //    //insertCmd.Parameters.AddWithValue("@University_Code", intern_list.University_Code);
-        //    //insertCmd.ExecuteNonQuery();
+            //SqlCommand insertCmd = new SqlCommand("INSERT INTO Intern(Name, University_Code, LastModifyDate) VALUES(@Name, @University_Code, getdate())");
+            //insertCmd.Connection = connection;
+            //insertCmd.Parameters.AddWithValue("@Name", intern_list.Name);
+            //insertCmd.Parameters.AddWithValue("@University_Code", intern_list.University_Code);
+            //insertCmd.ExecuteNonQuery();
 
-        //    listIntern = connection.Query<Intern>("SELECT TOP (1000) [Name],[University_Code] FROM [Intern] ORDER BY [LastModifyDate] ASC").ToList();
+            listInternStudent = connection.Query<InternStudent>("SELECT TOP (1000) [Name],[University_code] FROM [Intern] ORDER BY [LastModifyDate] ASC").ToList();
 
-        //    //SqlCommand queryCmd = new SqlCommand("SELECT TOP (1000) [Name],[University_Code] FROM [Intern] ORDER BY [LastModifyDate] ASC", connection);
-        //    //dr = queryCmd.ExecuteReader();
+            //SqlCommand queryCmd = new SqlCommand("SELECT TOP (1000) [Name],[University_Code] FROM [Intern] ORDER BY [LastModifyDate] ASC", connection);
+            //dr = queryCmd.ExecuteReader();
 
-        //    //while (dr.Read())
-        //    //{
-        //    //    listIntern.Add(new Intern() { Name = dr.GetString(0), University_Code = dr.GetString(1) });
-        //    //}
+            //while (dr.Read())
+            //{
+            //    listIntern.Add(new Intern() { Name = dr.GetString(0), University_Code = dr.GetString(1) });
+            //}
 
-        //    connection.Close();
+            connection.Close();
 
-        //    return View(listIntern);
-        //}
+            return View(listInternStudent);
+        }
 
 
-        ////// delete
-        //[HttpGet]
-        //[Route("InternList/Delete/{name}")]
-        //public IActionResult Delete(string name)
-        //{
-        //    string connectionstring = configuration.GetConnectionString("defaultConnectionString");
-        //    SqlConnection connection = new SqlConnection(connectionstring);
+        // getid
+        [HttpGet]
+        [Route("InternStudents/Get/{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var result = new InternStudent();
+            string connectionstring = configuration.GetConnectionString("defaultConnectionString");
+            SqlConnection connection = new SqlConnection(connectionstring);
 
-        //    connection.Open();
-        //    connection.Execute("DELETE FROM [Intern] WHERE Name=@Name",
-        //        new
-        //        {
-        //            Name = name
-        //        });
+            connection.Open();
+            SqlCommand queryCmd = new SqlCommand("select [Name] FROM [Intern] WHERE Name=@Name", connection);
+            queryCmd.Connection = connection;
+            queryCmd.Parameters.AddWithValue("@Name", name);
+            dr = queryCmd.ExecuteReader();
 
-        //    //SqlCommand delCommand = new SqlCommand("DELETE FROM [Intern] WHERE Name=@Name", connection);
-        //    //delCommand.Connection = connection;
-        //    //delCommand.Parameters.AddWithValue("@Name", name);
-        //    //delCommand.ExecuteNonQuery();
-        //    connection.Close();
-        //    return Ok("Successfully Deleted!");
-        //}
+            while (dr.Read())
+            {
+                result.Name = dr.GetString(0);
+            }
 
-        //// edit
-        //[HttpPost]
-        //public ActionResult Edit(Intern intern_list)
-        //{
-        //    string connectionstring = configuration.GetConnectionString("defaultConnectionString");
-        //    SqlConnection connection = new SqlConnection(connectionstring);
+            connection.Close();
 
-        //    connection.Open();
-        //    connection.Execute("UPDATE [Intern] SET[Name] = @Name,[University_code] = @University_code, [LastModifyDate] = getdate() WHERE Name=@Name",
-        //        new
-        //        {
-        //            Name = intern_list.Name,
-        //            University_Code = intern_list.University_Code,
-        //            LastModifyDate = intern_list.LastModifyDate
-        //        });
+            return Ok(result);
+        }
 
-        //    //SqlCommand editCommand = new SqlCommand("UPDATE [Intern] SET[Name] = @Name,[University_code] = @University_code, [LastModifyDate] = getdate() WHERE Name=@Name", connection);
-        //    //editCommand.Parameters.AddWithValue("@Name", university.Code);
-        //    //editCommand.Parameters.AddWithValue("@University_code", university.Universities);
-        //    //editCommand.ExecuteNonQuery();
 
-        //    connection.Query<Intern>("SELECT TOP (1000) [Name],[University_Code] FROM [Intern] ORDER BY [LastModifyDate] ASC").ToList();
+        // delete
+        [HttpGet]
+        [Route("InternStudents/Delete/{name}")]
+        public IActionResult delete(string name)
+        {
+            string connectionstring = configuration.GetConnectionString("defaultConnectionString");
+            SqlConnection connection = new SqlConnection(connectionstring);
 
-        //    //SqlCommand queryCmd = new SqlCommand("SELECT TOP (1000) [Name],[University_Code] FROM [Intern] ORDER BY [LastModifyDate] ASC", connection);
-        //    //dr = queryCmd.ExecuteReader();
+            connection.Open();
+            connection.Execute("DELETE FROM [University] WHERE Name=@Name",
+                new
+                {
+                    name = name
+                });
 
-        //    //while (dr.Read())
-        //    //{
-        //    //    listUniversity.Add(new University() { Code = dr.GetString(0), Universities = dr.GetString(1) });
-        //    //}
+            //sqlcommand delcommand = new sqlcommand("delete from [intern] where name=@name", connection);
+            //delcommand.connection = connection;
+            //delcommand.parameters.addwithvalue("@name", name);
+            //delcommand.executenonquery();
+            connection.Close();
+            return Ok("successfully deleted!");
+        }
 
-        //    connection.Close();
+        // edit
+        [HttpPost]
+        public ActionResult Edit(InternStudent intern_student)
+        {
+            string connectionstring = configuration.GetConnectionString("defaultConnectionString");
+            SqlConnection connection = new SqlConnection(connectionstring);
 
-        //    return Ok("Successfully Edited!");
-        //}
+            connection.Open();
+            connection.Execute("UPDATE [Intern] SET[Name] = @Name,[University_code] = @University_code, [LastModifyDate] = getdate() WHERE Name=@Name",
+                new
+                {
+                    Name = intern_student.Name,
+                    University_Code = intern_student.University,
+                    LastModifyDate = intern_student.LastModifyDate
+                });
+
+            //SqlCommand editCommand = new SqlCommand("UPDATE [Intern] SET[Name] = @Name,[University_code] = @University_code, [LastModifyDate] = getdate() WHERE Name=@Name", connection);
+            //editCommand.Parameters.AddWithValue("@Name", university.Code);
+            //editCommand.Parameters.AddWithValue("@University_code", university.Universities);
+            //editCommand.ExecuteNonQuery();
+
+            connection.Query<InternStudent>("SELECT [Name],[University_แode] FROM [Intern] ORDER BY [LastModifyDate] ASC").ToList();
+
+            //SqlCommand queryCmd = new SqlCommand("SELECT TOP (1000) [Name],[University_Code] FROM [Intern] ORDER BY [LastModifyDate] ASC", connection);
+            //dr = queryCmd.ExecuteReader();
+
+            //while (dr.Read())
+            //{
+            //    listUniversity.Add(new University() { Code = dr.GetString(0), Universities = dr.GetString(1) });
+            //}
+
+            connection.Close();
+
+            return Ok("Successfully Edited!");
+        }
 
     }
 }
